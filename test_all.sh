20d9e5bc451f48f1af803928b40b23886544afb6
Igor Rafael <igor.rafael@gmail.com>
2021-04-20 07:21:11 -0300

    chore: upgrade test scripts

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
 dialects=("postgres" "mysql" "mssql" "sqlite")
 
-for dialect in "${dialects[@]}" ; do
-    DEBUG=false GORM_DIALECT=${dialect} go test
-done
+function setup(){
+    docker-compose up -d
+}
+
+function all(){
+    setup;
+    for dialect in "${dialects[@]}" ; do
+        DEBUG=false GORM_DIALECT=${dialect} go test
+    done
+}
+
+function postgres(){
+    setup;
+    DEBUG=false GORM_DIALECT=postgres go test
+}
+
+eval "$@"
